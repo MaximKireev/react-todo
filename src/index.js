ad411be7085917512e1fce4bd17a3486c9ae48f7
MaximKireev <kyreyev.maksym@gmail.com>
2021-07-19 20:26:31 +0200

    first git commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let App = () => {
+    return (
+        <div>
+            <Header />
+            <Search />
+            <SearchItems />
+        </div>
+    )
+}
+const Header = () => {
+    return <h1>My ToDo list</h1>
+}
+const Search = () => {
+    return <input placeholder= 'Lets add new task!'/>
+}
+const SearchItems = () => {
+    let itemsArray = ["Dreenk coffe", 'Create App']
+    return (
+        <ul>
+            <li>{itemsArray[0]}</li>
+            <li>{itemsArray[1]}</li>
+        </ul>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
